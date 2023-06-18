2b95cf5a83fdb66a4fde8ef7f846226cdc7aeb9f
ian5030560 <110732376+ian5030560@users.noreply.github.com>
2023-06-18 19:30:02 +0800

    update

--- Anime1_flask/app/app.py
+++ Anime1_flask/app/app.py
@@ -4,9 +4,22 @@ from anime1.explorer import explore
 app = Flask(__name__)
 info = explore()
 
+def make_row(arg: list):
+    name = arg[1]
+    number = arg[2][3: len(arg[2]) - 1]
+    
+    result = "{}[{}]".format(name, number)
+
+    return result
+
+def make_link(number: int):
+    return "http://anime1.me/?cat={}".format(number)
+
+app.jinja_env.globals.update(make_row = make_row, make_link = make_link)
+
 @app.route("/")
 def main():
-    return render_template("main.html", info = info["content"][: 20], start = 0, end = 20, total = info["length"])
+    return render_template("main.html", info1 = info["content"][: 20], info2 = info["content"][: 10], start = 0, end = 20, total = info["length"])
 
 if __name__ == '__main__':
     app.run(debug=True)
